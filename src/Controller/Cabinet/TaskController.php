582c7ac1956b018a75bb8680ee8e5475d2015014
Konstantin Chumakov <kchumakov@ourgapps.com>
2024-04-16 00:05:46 +0300

    create task from web

--- src/Controller/Cabinet/TaskController.php
+++ src/Controller/Cabinet/TaskController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Cabinet;
 
+use App\Entity\Task;
+use App\Form\CreateTaskFromCabinetFormType;
 use App\Service\TaskService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
@@ -23,6 +27,30 @@ class TaskController extends AbstractController
 
         return $this->render('task/index.html.twig', [
             'activeTasks' => $activeTasks->tasks,
+        ]);
+    }
+
+    #[Route('/create-task', name: 'app_create_task')]
+    public function createTask(
+        #[CurrentUser] $user,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $task = new Task('', $user);
+        $form = $this->createForm(CreateTaskFromCabinetFormType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_tasks');
+        }
+
+        return $this->render('task/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/CreateTaskFromCabinetFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use App\Enum\PeriodEnum;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateTaskFromCabinetFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('period', EnumType::class, [
+                'class' => PeriodEnum::class,
+                'empty_data' => PeriodEnum::THIS_WEEK
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
